88872f7118d3c30c0318fa24225783ab54ac43d9
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-01-06 21:12:44 +0000

    Deploying to gh-pages from @ VuktorTS/goit-react-hw-05-movies@a2e709e31e6abf235e4eb0347c913b38c6fae2f3 🚀

---
+++ static/js/186.79f68a65.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[186],{186:function(e,i,t){t.r(i);t(791);var s=t(184);i.default=function(){return(0,s.jsx)("div",{children:"Reviews"})}}}]);
+//# sourceMappingURL=186.79f68a65.chunk.js.map
